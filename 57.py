0a001f63eea8e1e5022ed8fd14d98c6c443ab985
- <brianjflaherty@pm.me>
2021-04-04 23:48:24 -0400

    Problem 57.

--- 57.py
+++ 57.py
@@ -1,11 +1,17 @@
-# numerator = 1393
-# denominator = 985
-# cumulator = 1
+from decimal import *
+numerator = Decimal(1393)
+denominator = Decimal(985)
+cumulator = 1
 
-# for i in range(0, 992):
-#     numerator = round(numerator * (round(2**.5 + 1, 6)))
-#     denominator = round(denominator * (2**.5 + 1))
-#     if len(str(numerator)) > len(str(denominator)):
-#         cumulator += 1
+for i in range(0, 992):
+    
+    numerator = round(numerator * Decimal((round(2**.5 + 1, 6))))
+    denominator = round(denominator * Decimal((round(2**.5 + 1, 6))))
+    if len(str(numerator)) > len(str(denominator)):
+        cumulator += 1
 
-# print(cumulator)
+print(cumulator) # 153
+
+"""
+Using the decimal library to get around infinite float errors in Python...
+"""
